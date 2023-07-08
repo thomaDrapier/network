31e55ee5b141b426602ab4f17a2bf9405ddf09ed
Thomas Drapier <drapierthomas.ozanam@gmail.com>
2023-07-08 17:50:11 +0200

    class client

--- client.py
+++ client.py
@@ -1,29 +1,31 @@
 import socket
 import threading
 
-HOST = 'localhost'
-PORT = 5000
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+class client ():
+    HOST = 'localhost'
+    PORT = 5000
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def send_message():
-    while True:
-        texte = input ("")
-        socket.send(texte.encode())
+    def send_message(self):
+        while True:
+            texte = input ("")
+            self.socket.send(texte.encode())
 
-def received_message():
-    while True :
-        data = socket.recv(1096)
-        print(data.decode())
+    def received_message(self):
+        while True :
+            data = self.socket.recv(1096)
+            print(data.decode())
 
-try:
-    socket.connect((HOST, PORT))
-    print("Vous êtes connectés au serveur")
-    thread1 = threading.Thread(target=send_message, args=())
-    thread2 = threading.Thread(target=received_message, args=())
-    thread1.start()
-    thread2.start()
-except:
-    pass
-    
+    def connect_server(self):
+        try:
+            self.socket.connect((self.HOST, self.PORT))
+            thread1 = threading.Thread(target=self.send_message, args=())
+            thread2 = threading.Thread(target=self.received_message, args=())
+            thread1.start()
+            thread2.start()
+        except:
+            pass
+            
 
